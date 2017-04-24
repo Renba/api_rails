254bc77e997a5d683f41a2d5d20da1e3e455b9a0
renba <cs_abner@hotmail.com>
2017-04-24 14:28:45 -0500

    set rspec test & routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api,
             default: {format: :json},
             constraints: { subdomain: 'api'}, path:'/' do
+    scope module: :v1 do
 
+    end
   end
 
 end
