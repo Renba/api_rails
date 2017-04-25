2f7f4d96253b784e36a56bf2afa6107014c38f9c
renba <cs_abner@hotmail.com>
2017-04-25 12:43:34 -0500

    ads show action for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-
   devise_for :users
-  namespace :api,
-            default: {format: :json},
-            constraints: { subdomain: 'api'}, path:'/' do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1,
-          constraints: ApiConstraints.new(version: 1, default: true) do
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show]
     end
   end
-
 end
