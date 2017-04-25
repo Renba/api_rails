d1f09b4f3c09f2a54a6a0bbb24148c4dd733a0d9
renba <cs_abner@hotmail.com>
2017-04-25 11:13:13 -0500

    devise user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  devise_for :users
   namespace :api,
             default: {format: :json},
             constraints: { subdomain: 'api'}, path:'/' do
