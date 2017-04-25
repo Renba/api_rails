93a4a7d0ca58ad8c43ba01b0146e1b6bcd9e37d8
renba <cs_abner@hotmail.com>
2017-04-25 10:52:41 -0500

    api spec things

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
   namespace :api,
             default: {format: :json},
             constraints: { subdomain: 'api'}, path:'/' do
-    scope module: :v1 do
-
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
 
