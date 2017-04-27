83411d9a88201edac56c643d5675acc7ac554490
renba <cs_abner@hotmail.com>
2017-04-27 13:54:50 -0500

    destroy/update actions & specs

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -51,6 +51,52 @@ describe Api::V1::UsersController do
 
       it {should respond_with 422}
     end
+  end
+
+  describe "PUT/PATCH #update" do
+    context "when is successfuly updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, {id: @user.id,
+                        user:{ email: "newemail@example.com"} }, format: :json
+      end
+
+      it "render the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newemail@example.com"
+      end
+
+      it {should respond_with 200}
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+                        user:{ email: "badcorreo" } }, format: :json
+      end
+
+      it "render and errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it{ should respond_with 422 }
+    end
+  end
+
+  describe "when " do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      delete :destroy, { id: @user.id}, format: :json
+    end
+
+    it{ should respond_with 204 }
 
   end
 
